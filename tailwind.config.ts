7f33a3a7e368180d479d16a64d674e2c481dca41
Shirafzx <shirafzx@gmail.com>
2024-12-04 18:53:52 +0700

    Setup Light & Dark Theme, Shadcn, Navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -57,5 +58,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
